f3abb70d8f9b879703f13ebb5d6c7b696f2dd76e
Nguyen Hoang Minh <hoangminhlvt@gmail.com>
2026-01-05 20:48:53 +0800

    finished bitmap physical frame allocator

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,14 +2,11 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use kernel::mem::MAPPER;
 use kernel::{
     gdt, hlt_loop, interrupts,
     io::{framebuffer, serial_monitor},
     mem, println,
 };
-use x86_64::VirtAddr;
-use x86_64::structures::paging::Page;
 
 bootloader_api::entry_point!(kernel_start, config = &kernel::BOOTLOADER_CONFIG);
 #[unsafe(no_mangle)]
@@ -19,28 +16,9 @@ fn kernel_start(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     mem::init();
     gdt::init_gdt();
     interrupts::init_idt();
-
-    let mut frame_allocator = mem::EmptyFrameAllocator;
-
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
     unsafe {
-        let mapper = MAPPER.get_unchecked();
-
-        mem::create_example_mapping(page, &mut mapper.lock(), &mut frame_allocator);
-        let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
-
-        let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-        let a = page_ptr.offset(400).read();
-        println!("{a:#x}")
+        mem::frame_allocator::init_frame_allocator(&boot_info.memory_regions);
     }
-
-    // let handler = kernel::io::acpi::AcpiHandler::new(VirtAddr::new(PHYSICAL_MEMORY_OFFSET));
-    // let rdsp = boot_info.rsdp_addr.take().unwrap();
-    //
-    // unsafe {
-    //     let table = acpi::AcpiTables::from_rsdp(handler, rdsp as usize).unwrap();
-    // }
 
     hlt_loop()
 }
---
+++ kernel/src/mem/frame_allocator.rs
@@ -0,0 +1,109 @@
+use bootloader_api::info::MemoryRegionKind::Usable;
+use bootloader_api::info::MemoryRegions;
+use spin::{Mutex, Once};
+use x86_64::PhysAddr;
+use x86_64::structures::paging::{FrameAllocator, FrameDeallocator, PhysFrame, Size4KiB};
+
+pub static FRAME_ALLOCATOR: Once<Mutex<KernelFrameAllocator>> = Once::new();
+
+/// # Safety
+///
+/// The caller must ensure that all the Usable regions in memory_map is unused
+pub unsafe fn init_frame_allocator(memory_map: &'static MemoryRegions) {
+    FRAME_ALLOCATOR.call_once(|| unsafe { Mutex::new(KernelFrameAllocator::new(memory_map)) });
+}
+
+pub struct KernelFrameAllocator {
+    bitmap: &'static mut [u64],
+    max_frames: u64,
+    // optimization so that we do not search over already allocated frames
+    next_free_frame: u64,
+}
+
+impl KernelFrameAllocator {
+    /// # Safety
+    ///
+    /// The caller must ensure that all the Usable regions in memory_map is unused
+    pub unsafe fn new(memory_map: &'static MemoryRegions) -> Self {
+        let total_frames = memory_map.iter().last().unwrap().end / 4096;
+        let bitmap_size = total_frames.div_ceil(8);
+
+        let bitmap_memory_region = memory_map
+            .iter()
+            .find(|r| r.kind == Usable && (r.end - r.start) >= bitmap_size)
+            .expect("could not find a memory region large enough for the bitmap");
+
+        let bitmap_start_addr = bitmap_memory_region.start;
+
+        let bitmap_slice = unsafe {
+            core::slice::from_raw_parts_mut(bitmap_start_addr as *mut u64, bitmap_size as usize)
+        };
+
+        // start from safe state (everything is used)
+        bitmap_slice.fill(0xFF);
+
+        let mut allocator = Self {
+            bitmap: bitmap_slice,
+            max_frames: total_frames,
+            next_free_frame: 0,
+        };
+
+        memory_map
+            .iter()
+            .filter(|r| r.kind == Usable)
+            .for_each(|r| allocator.mark_range_as_free(r.start, r.end));
+
+        // mark the bitmap memory as used
+        allocator.mark_range_as_used(bitmap_start_addr, bitmap_start_addr + bitmap_size);
+        allocator.next_free_frame = (bitmap_start_addr + bitmap_size) / 4096;
+
+        allocator
+    }
+
+    fn mark_range_as_free(&mut self, start: u64, end: u64) {
+        for frame_addr in (start..end).step_by(4096) {
+            let frame_idx = (frame_addr / 4096) as usize;
+            self.bitmap[frame_idx / 64] &= !(1 << (frame_idx % 64));
+        }
+    }
+
+    fn mark_range_as_used(&mut self, start: u64, end: u64) {
+        for frame_addr in (start..end).step_by(4096) {
+            let frame_idx = (frame_addr / 4096) as usize;
+            self.bitmap[frame_idx / 64] |= 1 << (frame_idx % 64);
+        }
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for KernelFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        for frame_idx in self.next_free_frame..self.max_frames {
+            let byte_idx = frame_idx / 64;
+            let bit_idx = byte_idx % 64;
+
+            if (self.bitmap[byte_idx as usize] & (1 << bit_idx)) == 0 {
+                self.bitmap[byte_idx as usize] |= (1 << bit_idx);
+                self.next_free_frame += 1;
+
+                let addr = frame_idx * 4096;
+                return Some(PhysFrame::containing_address(PhysAddr::new(addr)));
+            }
+        }
+
+        None // no free frames
+    }
+}
+
+impl FrameDeallocator<Size4KiB> for KernelFrameAllocator {
+    unsafe fn deallocate_frame(&mut self, frame: PhysFrame<Size4KiB>) {
+        let frame_idx = frame.start_address().as_u64() / 4096;
+        let byte_idx = frame_idx / 64;
+        let bit_idx = byte_idx % 64;
+
+        self.bitmap[byte_idx as usize] &= !(1 << bit_idx);
+
+        if frame_idx < self.next_free_frame {
+            self.next_free_frame = frame_idx
+        }
+    }
+}
